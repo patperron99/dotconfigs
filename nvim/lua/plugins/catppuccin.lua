a3936e16571bf6a4550935741ccc86fa2ffc42b7
sysadmin <sysadmin@debian-strap.localdomain>
2025-01-08 08:27:14 -0500

    add nvim

---
+++ nvim/lua/plugins/catppuccin.lua
@@ -0,0 +1,12 @@
+return {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    config = function()
+      require("catppuccin").setup({
+        transparent_background = true,  -- Enable transparency
+      })
+      vim.cmd.colorscheme "catppuccin"
+    end
+}
+
+
